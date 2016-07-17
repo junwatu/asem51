4b264da336f7f961f2a828f9e677c79312ac6b89
Equan Pr <equan.p@gmail.com>
2016-07-18 06:14:29 +0700

    Add test for boolean args

--- test/test.js
+++ test/test.js
@@ -4,11 +4,19 @@ import ASEM51 from '../lib/asem51';
 import assert from 'assert';
 
 const args = [null, null, '-o', '-v', 'test/main.a51'];
+const errExpected = [
+    { file: 'test/main.a51', line: '1', column: '6', message: ' illegal operand' },
+    { file: 'test/main.a51', line: '2', column: '10', message: ' no END statement found' }
+]
+
 
 describe('Args', () => {
     it('Test Boolean Args', () => {
         ASEM51.asyncAsem(args)
-            .then(data => console.log(data))
+            .then(data => { 
+                assert.equal(data[0].file, errExpected[0].file);
+                assert.equal(data[1].column, errExpected[1].column); 
+            })
             .catch(err => console.log(err));
     })
 })
