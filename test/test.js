0c5b97823e268cf50c997736628dcef01866bc57
Equan Pr <equan.p@gmail.com>
2016-07-18 07:22:14 +0700

    Change test according to new output format from asem51

--- test/test.js
+++ test/test.js
@@ -5,17 +5,18 @@ import assert from 'assert';
 
 const args = [null, null, '-o', '-v', 'test/main.a51'];
 const errExpected = [
-    { file: 'test/main.a51', line: '1', column: '6', message: ' illegal operand' },
-    { file: 'test/main.a51', line: '2', column: '10', message: ' no END statement found' }
+    { file: 'test/main.a51', line: '1', column: '6', message: 'illegal operand' },
+    { file: 'test/main.a51', line: '2', column: '10', message: 'no END statement found' }
 ]
 
 
-describe('Args', () => {
-    it('Test Boolean Args', () => {
+describe('Args Test', () => {
+    it('Boolean', () => {
         ASEM51.asyncAsem(args)
             .then(data => { 
-                assert.equal(data[0].file, errExpected[0].file);
-                assert.equal(data[1].column, errExpected[1].column); 
+                console.log(data)
+                assert.equal(data[1][0].file, errExpected[0].file);
+                assert.equal(data[1][1].column, errExpected[1].column); 
             })
             .catch(err => console.log(err));
     })
